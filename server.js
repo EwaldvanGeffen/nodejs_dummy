4a70dcfa87f7a8c52d6cd65085f84c0357a3f66b
Ewald van Geffen <ewald.vangeffen@nucleus.be>
2017-01-09 10:34:33 +0100

    BUG: concat strings with + not dots

--- server.js
+++ server.js
@@ -3,4 +3,4 @@ http.createServer(function (req, res) {
       res.writeHead(200, {'Content-Type': 'text/plain'});
         res.end('Hello World\n');
 }).listen(process.env.PORT);
-console.log('Server listening on port ' . process.env.PORT);
+console.log('Server listening on port ' + process.env.PORT);
